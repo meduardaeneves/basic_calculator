d6000c73ab36fa1d251e45ccefabcd1e06f946a2
Maria Eduarda Neves <mariaeduardaeneves@gmail.com>
2025-05-30 19:47:29 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,52 @@
 class Calculator {
     constructor() {
-        this.upperValue = 0;
-        this.resultValue = 0;
+        this.upperValue = document.querySelector('#upper-number');
+        this.resultValue = document.querySelector('#result-number');
         this.reset = 0;
     }
 
+    checkLastDigit(input,upperValue,reg) {
+        if (!reg.test(input) && //verifica se o input não é Num
+            //verifica se o último elemento no display tb não é num
+            !reg.test(upperValue.substr(upperValue.length-1))) {
+               return true 
+        } else {
+            return false
+
+        }
+    }
+
+    btnPress () {
+        let input = this.textContent;
+        let upperValue = calc.upperValue.textContent;
+        // verificar se tem so numeros
+        var reg = new RegExp('^[0-9]+$')
+
+        //Verifica se precisa adicionar ou não:
+        // se meu último dígito for (+;-;/;*) eu vou substitur ele pelo novo apertado
+        if (calc.checkLastDigit(input,upperValue,reg)) {
+            calc.upperValue.textContent = upperValue.slice(0, -1);
+        }
+
+
+        if (upperValue == '0') {
+            calc.upperValue.textContent = input;
+        } else {
+            calc.upperValue.textContent += input;
+        }
+        
+    }
 }
+
+// start obj
+let calc = new Calculator()
+
+// start btns
+let buttons = document.querySelectorAll('.btn')
+
+// map all btns --> mapear todos botões
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click',calc.btnPress)
+}
+
+
